04b2e78f8b991774b9712837d68813521195090a
Prabhat Sharma <hi.prabhat@gmail.com>
2023-03-02 00:00:32 +0530

    upload etcd as dependency

--- publish.sh
+++ publish.sh
@@ -11,6 +11,7 @@ mkdir artifacts
 cp -r index.yaml artifacts/index.yaml
 cp -r *.tgz artifacts/
 aws s3 sync artifacts s3://zincsearch-releases/charts/
+aws s3 sync charts s3://zincsearch-releases/charts/charts/
 
 # invalidate cludfront cache
 aws cloudfront create-invalidation --distribution-id E1MZFY4I0CFJV9 --paths=/*
