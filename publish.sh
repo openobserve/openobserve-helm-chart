821db0c69774df96cfab69b6924e23b5d11da598
Prabhat Sharma <hi.prabhat@gmail.com>
2023-03-01 13:56:41 +0530

    fixed upload of chart

--- publish.sh
+++ publish.sh
@@ -4,11 +4,10 @@ helm package .
 
 helm repo index --url https://charts.zinc.dev .
 
-aws s3 cp index.yaml s3://zincsearch-releases/charts/index.yaml
-
-aws s3 cp *.tgz s3://zincsearch-releases/charts/
+cp -r index.yaml artifacts/index.yaml
+cp -r *.tgz artifacts/
+aws s3 sync artifacts s3://zincsearch-releases/charts/
 
 # invalidate cludfront cache
-
 aws cloudfront create-invalidation --distribution-id E1MZFY4I0CFJV9 --paths=/*
 
