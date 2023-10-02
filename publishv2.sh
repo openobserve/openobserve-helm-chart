d33a818101cc18aa44fb0c23f045f19e56745fa5
Prabhat Sharma <hi.prabhat@gmail.com>
2023-10-02 16:03:06 -0700

    1 repo for openobserve and openobserve-collector

---
+++ publishv2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Package all charts
+for d in charts/* ; do
+    helm dependency update "$d"
+    helm package "$d"
+done
+
+# Create the Helm Repo Index and merge it with the existing index.yaml
+helm repo index --merge index.yaml .
+
+# Upload the charts to the S3 bucket
+aws s3 cp . s3://zincsearch-releases/chartsv2/ --recursive --exclude "*" --include "*.tgz"
+
+# delete the charts after upload
+rm *.tgz
+
+# upload the index.yaml
+aws s3 cp index.yaml s3://zincsearch-releases/chartsv2/ --profile=dev
+
+# invalidate cludfront cache
+aws cloudfront create-invalidation --distribution-id E1KAOPVKDAGD4X --paths="/*" --profile=dev
