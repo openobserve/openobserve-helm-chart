778ac7d693889e52b7609288b860dcacca131038
Prabhat Sharma <hi.prabhat@gmail.com>
2024-11-30 07:34:56 -0800

    fixed SMTP

--- publishv2.sh
+++ publishv2.sh
@@ -10,13 +10,13 @@ done
 helm repo index --merge index.yaml .
 
 # Upload the charts to the S3 bucket
-aws s3 cp . s3://zincsearch-releases/chartsv2/ --recursive --exclude "*" --include "*.tgz" --profile=dev
+aws s3 cp . s3://zincsearch-releases/chartsv2/ --recursive --exclude "*" --include "*.tgz" --profile=o2-dev
 
 # delete the charts after upload
 rm *.tgz
 
 # upload the index.yaml
-aws s3 cp index.yaml s3://zincsearch-releases/chartsv2/ --profile=dev
+aws s3 cp index.yaml s3://zincsearch-releases/chartsv2/ --profile=o2-dev
 
 # invalidate cludfront cache
-aws cloudfront create-invalidation --distribution-id E1KAOPVKDAGD4X --paths="/*" --profile=dev
+aws cloudfront create-invalidation --distribution-id E1KAOPVKDAGD4X --paths="/*" --profile=o2-dev
